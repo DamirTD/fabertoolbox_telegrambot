f5a88c2b7d2d1758bd91757364b28a02ce9299ad
damir toriya <toriya.damir.2004@gmail.com>
2024-12-16 12:09:03 +0500

    Keyboard add

--- internal/bot/service.go
+++ internal/bot/service.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"fabertoolbox_bot/internal/bot/handlers"
+	"fabertoolbox_bot/internal/ui"
 	"gopkg.in/telebot.v4"
 	"sync"
 )
@@ -8,10 +10,15 @@ import (
 type Service struct {
 	Bot      *telebot.Bot
 	userInfo sync.Map
+	Menu     *telebot.ReplyMarkup
 }
 
 func (s *Service) RegisterHandlers() {
-	s.Bot.Handle("/start", func(c telebot.Context) error {
-		return s.handleStart(c)
-	})
+	s.Menu = ui.CreateMainMenu()
+
+	s.Bot.Handle("/start", handlers.HandleStartCommand(s.Menu))
+	s.Bot.Handle("О каналах", handlers.HandleAboutChannelsButton)
+	s.Bot.Handle("Подать заявку на вступление", handlers.HandleJoinRequestButton)
+	s.Bot.Handle("Оплатить подписку", handlers.HandleSubscriptionPaymentButton)
+	s.Bot.Handle("Задать вопрос", handlers.HandleAskQuestionButton)
 }
